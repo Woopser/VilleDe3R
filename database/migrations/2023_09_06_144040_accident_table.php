fb4fdae283d683e4edb39fb9a85ee8d067e52a6d
thony311 <antoinelefebvrepoupa@hotmail.com>
2023-09-06 10:53:10 -0400

    debut des migrations
    j'ai commencé les migrations des tables à partir du shéma uml modifier

--- database/migrations/2023_09_06_144040_accident_table.php
+++ database/migrations/2023_09_06_144040_accident_table.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('formulaire', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->dateTime('dateAccident');
+            $table->text('description');
+            $table->string('temoin')->nullable($value = true);
+            $table->string('nature');
+            $table->text('descriptionBlessure');
+            $table->string('violence');
+            $table->
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
