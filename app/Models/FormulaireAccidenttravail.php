3079bc2f7255f8d70cad1865502eb7f31c0cc444
thony311 <antoinelefebvrepoupa@hotmail.com>
2023-09-13 10:35:00 -0400

    asd

--- app/Models/FormulaireAccidenttravail.php
+++ app/Models/FormulaireAccidenttravail.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class FormulaireAccidenttravail extends Model
+class FormulaireAccidentTravail extends Model
 {
     use HasFactory;
 }
