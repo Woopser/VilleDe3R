60a36f1f6adb22c5261c08ac078d93f170f0aa64
ScribeQc <93779862+ScribeQc@users.noreply.github.com>
2023-09-13 10:38:36 -0400

    layout done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,11 @@
     <!-- Navbar -->
     <nav>
         <a id="nav-logo" href=""><img src="{{ asset('img/logo_v3r_n_et_blanc.svg') }}" alt="Logo" width="60"></a>
-        <a id="nav-hamburger" href="">x</a>
+        <a id="nav-hamburger" href="javascript:void(0);" class="icon" onclick="toggleMenu()">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+        </a>
         <div id="nav-links">
             <a class="nav-link" href="">Accueil</a>
             <a class="nav-link" href="">À propos</a>
@@ -23,5 +27,11 @@
     @yield('content')
 
     <!-- Footer -->
+    <footer>
+        <div>
+            
+        </div>
+    </footer>
 </body>
 </html>
+<script src="{{ asset('js/ville3R.js') }}"></script>
