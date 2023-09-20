5dc891dbf2d25788b170c1333f9c2b045d96ff85
thony311 <antoinelefebvrepoupa@hotmail.com>
2023-09-20 10:45:42 -0400

    test pour le javascript

--- public/js/declarationAccident.js
+++ public/js/declarationAccident.js
@@ -1,8 +1,9 @@
-let radioOui = document.getElementById('radioOui');
+/*let radioOui = document.getElementById('radioOui');
 let radioNon = document.getElementById('radioNon');
 
 radioOui.addEventListener('change', function(){
     if(this.checked){
-        
+        console.log("Checkbox is checked..")
     }
 });
+*/
--- resources/views/form/declarationAccident.blade.php
+++ resources/views/form/declarationAccident.blade.php
@@ -5,8 +5,15 @@
     <div style="height: 115px"></div>
     <div>
         <div>
+            <div class="modal" id="yesNoModal">
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <p>Some text in the Modal..</p>
+                </div>
+            </div>
             <form action="">
                 @csrf
+                <!--
                 <label class="form-label d-flex justify-content-center">Es se que vous êtes la personne blesser ?</label>
                 <div class="d-flex justify-content-center">
                     <div class="form-check form-check-inline">
@@ -18,7 +25,10 @@
                         <label class="form-check-label">Non</label>
                     </div>
                 </div>
-
+                 div d'ajout par javascript 
+                <div id="ajoutJava" ></div>
+                -->
+                
             </form>
         </div>
     </div>
