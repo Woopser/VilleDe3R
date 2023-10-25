68d41d3780c47f6bcf4db8a7e0ca60a35d9869ab
Thomas DR <petittom222@gmail.com>
2023-10-25 10:51:08 -0400

    sa push pour la journées
    mais faut je change la value partout

--- resources/views/form/audit.blade.php
+++ resources/views/form/audit.blade.php
@@ -8,7 +8,7 @@
 
     <h2>Formulaire d'audit SST</h2>
 
-    <form method="POST" >
+    <form method="POST" action="{{route('audit.store')}}">
         @csrf
 
         <div class="mb-3">
@@ -29,99 +29,99 @@
             <label class="form-check-label" for="flexRadioDefault2" >
                 EPI
             </label>
-            <input type="radio" name="epi" value="0" class="form-check-input" id="spanGauche">
+            <input type="radio" name="EPI" value="0" class="form-check-input" id="spanGauche">
 
-            <input type="radio" name="epi" value="1" class="form-check-input" id="spanGauche">
+            <input type="radio" name="EPI" value="1" class="form-check-input" id="spanGauche">
     
-            <input type="radio" name="epi" value="2" class="form-check-input" id="spanGauche">
+            <input type="radio" name="EPI" value="2" class="form-check-input" id="spanGauche">
         </div>
 
         <div>
             <label class="form-check-label" for="flexRadioDefault2" >
                 Tenue des lieux
             </label>
-            <input type="radio" name="epi" value="0" class="form-check-input" id="spanGauche">
+            <input type="radio" name="tenueLieux" value="0" class="form-check-input" id="spanGauche">
 
-            <input type="radio" name="epi" value="1" class="form-check-input" id="spanGauche">
+            <input type="radio" name="tenueLieux" value="1" class="form-check-input" id="spanGauche">
     
-            <input type="radio" name="epi" value="2" class="form-check-input" id="spanGauche">
+            <input type="radio" name="tenueLieux" value="2" class="form-check-input" id="spanGauche">
         </div>
 
         <div>
             <label class="form-check-label" for="flexRadioDefault2" >
                 Comportement sécuritaire
             </label>
-            <input type="radio" name="epi" value="0" class="form-check-input" id="spanGauche">
+            <input type="radio" name="comportement" value="0" class="form-check-input" id="spanGauche">
 
-            <input type="radio" name="epi" value="1" class="form-check-input" id="spanGauche">
+            <input type="radio" name="comportement" value="1" class="form-check-input" id="spanGauche">
     
-            <input type="radio" name="epi" value="2" class="form-check-input" id="spanGauche">
+            <input type="radio" name="comportement" value="2" class="form-check-input" id="spanGauche">
         </div>
 
         <div>
             <label class="form-check-label" for="flexRadioDefault2" >
                 Signalisation
             </label>
-            <input type="radio" name="epi" value="0" class="form-check-input" id="spanGauche">
+            <input type="radio" name="signalisation" value="0" class="form-check-input" id="spanGauche">
 
-            <input type="radio" name="epi" value="1" class="form-check-input" id="spanGauche">
+            <input type="radio" name="signalisation" value="1" class="form-check-input" id="spanGauche">
     
-            <input type="radio" name="epi" value="2" class="form-check-input" id="spanGauche">
+            <input type="radio" name="signalisation" value="2" class="form-check-input" id="spanGauche">
         </div>
 
         <div>
             <label class="form-check-label" for="flexRadioDefault2" >
                 Fiches Signalétiques
             </label>
-            <input type="radio" name="epi" value="0" class="form-check-input" id="spanGauche">
+            <input type="radio" name="ficheSignaletique" value="0" class="form-check-input" id="spanGauche">
 
-            <input type="radio" name="epi" value="1" class="form-check-input" id="spanGauche">
+            <input type="radio" name="ficheSignaletique" value="1" class="form-check-input" id="spanGauche">
     
-            <input type="radio" name="epi" value="2" class="form-check-input" id="spanGauche">
+            <input type="radio" name="ficheSignaletique" value="2" class="form-check-input" id="spanGauche">
         </div>
 
         <div>
             <label class="form-check-label" for="flexRadioDefault2" >
                 Travaux - Excabvation
             </label>
-            <input type="radio" name="epi" value="0" class="form-check-input" id="spanGauche">
+            <input type="radio" name="travaux" value="0" class="form-check-input" id="spanGauche">
 
-            <input type="radio" name="epi" value="1" class="form-check-input" id="spanGauche">
+            <input type="radio" name="travaux" value="1" class="form-check-input" id="spanGauche">
     
-            <input type="radio" name="epi" value="2" class="form-check-input" id="spanGauche">
+            <input type="radio" name="travaux" value="2" class="form-check-input" id="spanGauche">
         </div>
 
         <div>
             <label class="form-check-label" for="flexRadioDefault2" >
                 Espace clos
             </label>
-            <input type="radio" name="epi" value="0" class="form-check-input" id="spanGauche">
+            <input type="radio" name="espaceClos" value="0" class="form-check-input" id="spanGauche">
 
-            <input type="radio" name="epi" value="1" class="form-check-input" id="spanGauche">
+            <input type="radio" name="espaceClos" value="1" class="form-check-input" id="spanGauche">
     
-            <input type="radio" name="epi" value="2" class="form-check-input" id="spanGauche">
+            <input type="radio" name="espaceClos" value="2" class="form-check-input" id="spanGauche">
         </div>
 
         <div>
             <label class="form-check-label" for="flexRadioDefault2" >
                 Méthode de travail
             </label>
-            <input type="radio" name="epi" value="0" class="form-check-input" id="spanGauche">
+            <input type="radio" name="methode" value="0" class="form-check-input" id="spanGauche">
 
-            <input type="radio" name="epi" value="1" class="form-check-input" id="spanGauche">
+            <input type="radio" name="methode" value="1" class="form-check-input" id="spanGauche">
     
-            <input type="radio" name="epi" value="2" class="form-check-input" id="spanGauche">
+            <input type="radio" name="methode" value="2" class="form-check-input" id="spanGauche">
         </div>
 
         <div>
             <label class="form-check-label" for="flexRadioDefault2" >
                 Autre(s) (optionnel)
             </label>
-            <input type="radio" name="epi" value="0" class="form-check-input" id="spanGauche">
+            <input type="radio" name="autres" value="0" class="form-check-input" id="spanGauche">
 
-            <input type="radio" name="epi" value="1" class="form-check-input" id="spanGauche">
+            <input type="radio" name="autres" value="1" class="form-check-input" id="spanGauche">
     
-            <input type="radio" name="epi" value="2" class="form-check-input" id="spanGauche">
+            <input type="radio" name="autres" value="2" class="form-check-input" id="spanGauche">
         </div>
             
  
@@ -130,11 +130,48 @@
             <input type="text" class="form-control" id="numUnit" name="lieu">
         </div>
 
+
+        <div>
+            <label class="form-check-label" for="flexRadioDefault2" >
+                Distanciation Social
+            </label>
+            <input type="radio" name="distanciation" value="0" class="form-check-input" id="spanGauche">
+
+            <input type="radio" name="distanciation" value="1" class="form-check-input" id="spanGauche">
+    
+            <input type="radio" name="distanciation" value="2" class="form-check-input" id="spanGauche">
+        </div>
+
+        <div>
+            <label class="form-check-label" for="flexRadioDefault2" >
+                Port des  EPI (masques/visières)
+            </label>
+            <input type="radio" name="portMasque" value="0" class="form-check-input" id="spanGauche">
+
+            <input type="radio" name="portMasque" value="1" class="form-check-input" id="spanGauche">
+    
+            <input type="radio" name="portMasque" value="2" class="form-check-input" id="spanGauche">
+        </div>
+
+        <div>
+            <label class="form-check-label" for="flexRadioDefault2" >
+                Respect des procédures établis 
+            </label>
+            <input type="radio" name="respectProcedure" value="0" class="form-check-input" id="spanGauche">
+
+            <input type="radio" name="respectProcedure" value="1" class="form-check-input" id="spanGauche">
+    
+            <input type="radio" name="respectProcedure" value="2" class="form-check-input" id="spanGauche">
+        </div>
+
         <div class="mb-3">
             <label for="numImp" class="form-label">Description:  :</label>
             <textarea name="lieu"> </textarea>
         </div>
 
+        <div class="col-12">
+            <button class="btn btn-primary" type="submit">Envoyer</button>
+        </div>
 
 
     </form>
