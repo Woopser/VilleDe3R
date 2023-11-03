83f84977821bca333651e8bfc3a311933642ad69
ScribeQc <93779862+ScribeQc@users.noreply.github.com>
2023-11-03 14:07:15 -0400

    Form front-end update
    Manque des margins/paddings et le footer s'est volatilisé (le height est 0)

--- resources/views/form/audit.blade.php
+++ resources/views/form/audit.blade.php
@@ -1,183 +1,104 @@
 @extends('layouts.app')
-
 @section('V3R')
-
 @section('content')
-    
-
-
-    <h2>Formulaire d'audit SST</h2>
-
-    <form method="POST" action="{{route('audit.store')}}">
-        @csrf
-
-        <div class="mb-3">
-            <label for="numImp" class="form-label">Lieu des travaux :</label>
-            <input type="text" class="form-control" id="numUnit" name="lieu">
-        </div>
-
-        <div class="mb-3">
-            <label for="numImp" class="form-label">Date et heure :</label>
-            <input type="datetime-local" class="form-control" id="numUnit" name="date">
-        </div>
-        <div>
-            <span> 
-                <span id="spanGauche">      Conforme      </span> <span> Non-Conforme      </span> <span> N/A </span>
-            </span>
-        </div>
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                EPI
-            </label>
-            <input type="radio" name="EPI" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="EPI" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="EPI" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Tenue des lieux
-            </label>
-            <input type="radio" name="tenueLieux" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="tenueLieux" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="tenueLieux" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Comportement sécuritaire
-            </label>
-            <input type="radio" name="comportement" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="comportement" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="comportement" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Signalisation
-            </label>
-            <input type="radio" name="signalisation" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="signalisation" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="signalisation" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Fiches Signalétiques
-            </label>
-            <input type="radio" name="ficheSignaletique" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="ficheSignaletique" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="ficheSignaletique" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Travaux - Excabvation
-            </label>
-            <input type="radio" name="travaux" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="travaux" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="travaux" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Espace clos
-            </label>
-            <input type="radio" name="espaceClos" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="espaceClos" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="espaceClos" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Méthode de travail
-            </label>
-            <input type="radio" name="methode" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="methode" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="methode" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Autre(s) (optionnel)
-            </label>
-            <input type="radio" name="autres" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="autres" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="autres" value="2" class="form-check-input" id="spanGauche">
-        </div>
-            
- 
-        <div class="mb-3">
-            <label for="numImp" class="form-label">Autre(s) : Veuillez préciser</label>
-            <input type="text" class="form-control" id="numUnit" name="lieu">
-        </div>
-
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Distanciation Social
-            </label>
-            <input type="radio" name="distanciation" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="distanciation" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="distanciation" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Port des  EPI (masques/visières)
-            </label>
-            <input type="radio" name="portMasque" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="portMasque" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="portMasque" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div>
-            <label class="form-check-label" for="flexRadioDefault2" >
-                Respect des procédures établis 
-            </label>
-            <input type="radio" name="respectProcedure" value="0" class="form-check-input" id="spanGauche">
-
-            <input type="radio" name="respectProcedure" value="1" class="form-check-input" id="spanGauche">
-    
-            <input type="radio" name="respectProcedure" value="2" class="form-check-input" id="spanGauche">
-        </div>
-
-        <div class="mb-3">
-            <label for="numImp" class="form-label">Description:  :</label>
-            <textarea name="lieu"> </textarea>
-        </div>
-
-        <div class="col-12">
+    <div class="form-main">
+        <h2>Formulaire d'audit SST</h2>
+        <form method="POST" action="{{route('audit.store')}}">
+            @csrf
+            <table>
+                <tr>
+                    <td colspan="3"><input type="text" class="form-control" name="lieu" placeholder="Lieu des travaux"></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><input type="datetime-local" class="form-control" name="date"></td>
+                </tr>
+                <tr>
+                    <th></th>
+                    <th>Conforme</th>
+                    <th>Non-Conforme</th>
+                    <th>N/A</th>
+                </tr>
+                <tr>
+                    <td>EPI</th>
+                    <td><input type="radio" name="EPI" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="EPI" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="EPI" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td>Tenue des lieux</th>
+                    <td><input type="radio" name="tenueLieux" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="tenueLieux" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="tenueLieux" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td>Comportement sécuritaire</th>
+                    <td><input type="radio" name="comportement" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="comportement" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="comportement" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td>Signalisation</td>
+                    <td><input type="radio" name="signalisation" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="signalisation" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="signalisation" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td>Fiches Signalétiques</td>
+                    <td><input type="radio" name="ficheSignaletique" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="ficheSignaletique" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="ficheSignaletique" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td>Travaux - Excavation</td>
+                    <td><input type="radio" name="travaux" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="travaux" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="travaux" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td>Espace clos</td>
+                    <td><input type="radio" name="espaceClos" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="espaceClos" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="espaceClos" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td>Méthode de travail</td>
+                    <td><input type="radio" name="methode" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="methode" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="methode" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td>Autres (optionnel)</td>
+                    <td><input type="radio" name="autres" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="autres" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="autres" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    
+                    <td colspan="3"><input type="text" class="form-control" placeholder="Veuillez préciser" name="lieu"></td>
+                </tr>
+                <tr>
+                    <td>Distanciation Sociale</td>
+                    <td><input type="radio" name="distanciation" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="distanciation" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="distanciation" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td>Port des EPI (masques/visières)</td>
+                    <td><input type="radio" name="portMasque" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="portMasque" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="portMasque" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td>Respect des procédures établis</td>
+                    <td><input type="radio" name="respectProcedure" value="0" class="form-check-input"></td>
+                    <td><input type="radio" name="respectProcedure" value="1" class="form-check-input"></td>
+                    <td><input type="radio" name="respectProcedure" value="2" class="form-check-input"></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><textarea name="lieu"></textarea></td>
+                </tr>
+            </table>
             <button class="btn btn-primary" type="submit">Envoyer</button>
-        </div>
-
-
-    </form>
-
-
-
-
-    
+        </form>
+    </div>
 @endsection
