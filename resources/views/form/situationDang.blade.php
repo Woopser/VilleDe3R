6e70edfb6be85bbc21549a2d43a9536953a4d6eb
Thomas DR <petittom222@gmail.com>
2023-11-03 14:26:56 -0400

    sa push pour le commencement du dernier form

---
+++ resources/views/form/situationDang.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('V3R')
+
+@section('content')
+
+@if(isset($errors) && $errors->any())
+
+     <div class="alert alert-danger">
+
+         @foreach($errors->all() as $error)
+
+             <p>{{$error}}</p>
+
+        @endforeach
+
+    </div>
+@endif
+<div style="height: 100.05px"></div>
+<h1>Formulaire de signalement de situation dangeureuse</h1>
+
+<form method="POST" action="">
+    @csrf
+
+</form>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,7 @@ use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\AuditsController;
 use App\Http\Controllers\FormulaireController;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\SituationDangController;
 
 //Route pour formulaire accident de voiture
 Route::get('/formulaires/accidentVoiture',
@@ -49,6 +38,9 @@ Route::get('/formulaire/declarationAccident', [FormulaireAccidentTravailControll
 Route::POST('/formulaire/declarationAccident/store',[FormulaireAccidentTravailController::class, 'store'])->name('formulaireAccidentTravail.store');
 
 
-//-----------------Route pour Grille Audit SST
+//-----------------Route pour Grille Audit SST-------------------------------------------------//
 Route::get('/formulaires/audit', [AuditsController::class, 'index'])->name('formulaireAudit');
 Route::POST('formulaire/audit/store', [AuditsController::class, 'store'])->name('audit.store');
+
+//-----Route pour Situations dangeureuse-----------//
+Route::get('formulaires/situationDang',[SituationDangController::class, 'index'])->name('formulaireSit');
