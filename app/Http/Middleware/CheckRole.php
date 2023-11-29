384ad7b84b67ce4718649036a553f92e784638a6
thony311 <antoinelefebvrepoupa@hotmail.com>
2023-11-29 10:51:16 -0500

    middlewar parsiellement fait
    marche il manque de le faire sur les notiicaiton et les trucs que thomas fessait aujourd'hui 2023-11-29

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -5,7 +5,9 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 class CheckRole
 {
     /**
@@ -13,25 +15,24 @@ class CheckRole
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string $roles): Response
+    public function handle(Request $request, Closure $next, ...$roles)
     {
-
         
-        try{// Vérifie si l'utilisateur est connecté
-             // vérifie les rôles
-             $userRole = explode(',', auth()->user()->role);
-             foreach($roles as $role){
-                if(in_array($role, $userRole)){
-                    return $next($request);
-                }
-             }
-        }catch(\Throwable $e){ // Sinon redirige vers une pages avec erreurs
-            Log::debug($e);
-            return redirect()->route('users.index')->withErrors(['Vous n\'avez pas accès à cette page']);
+        if (Session::get('matricule') == Null)
+        {
+            return redirect()->route('users.index')->withErrors(['Accès interdit']);
         }
-        
-
-
+        // Vérifier si l'utilisateur courant a un des rôles autorisés
+        foreach ($roles as $role) // pour chaque role rentrer dedans le middleware il va
+        {
+            Log::debug("dedans le foreach");
+            if ($role == Session::get('role')); // regarder si l'employe est un de ceux ci
+            {
+                return $next($request); // si oui il retourne la request qu'il doit faire
+            }
+        }
+            // Sinon, rediriger vers une page d'erreur
+           return redirect()->route('users.index')->withErrors(['Vous n\'avez pas l\'accès a cette page']);
         
     }
 }
