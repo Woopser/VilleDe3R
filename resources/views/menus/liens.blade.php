d981568b1766553572e01d58001075909109ee9e
ScribeQc <93779862+ScribeQc@users.noreply.github.com>
2023-12-05 17:13:35 -0500

    target blank

--- resources/views/menus/liens.blade.php
+++ resources/views/menus/liens.blade.php
@@ -4,12 +4,11 @@
 
 <div class="notif-main">
    <h1>Liens utiles</h1>
-   <h3> {{$lian->nom}} </h3>
    <table class="notif-table">
       @foreach($liens as $lien)
          <tr>
                <td>
-                  <a href="{{$lien->lien}}">{{$lien->nom}}</a>
+                  <a href="{{$lien->lien}}" target="blank">{{$lien->nom}}</a>
                </td>
          </tr>
       @endforeach
